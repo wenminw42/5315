793858b39c016a26455fc78d6a5e0e3c6f56a97b
wangw4 <wangw4@seattleu.edu>
2018-10-15 23:03:43 -0700

    hw files

---
+++ customerdata.js
@@ -0,0 +1,124 @@
+{
+  "data": [
+    {
+      "customer_name": "Julina Smith",
+      "purchase_date": "1/1/2018",
+      "membership": "Yes",
+      "city": "Portland",
+      "state": "OR",
+      "amount": "$20.50"
+    },
+    {
+      "customer_name": "Mark Well",
+      "purchase_date": "11/12/2018",
+      "membership": "Yes",
+      "city": "Portland",
+      "state": "OR",
+      "amount": "$342.09"
+    },
+    {
+      "customer_name": "Mary Volve",
+      "purchase_date": "3/18/2018",
+      "membership": "No",
+      "city": "Yakima",
+      "state": "WA",
+      "amount": "$31.82"
+    },
+    {
+      "customer_name": "Connie Lake",
+      "purchase_date": "6/19/2018",
+      "membership": "Yes",
+      "city": "Olympia",
+      "state": "WA",
+      "amount": "$200.50"
+    },
+    {
+      "customer_name": "Jay Pieth",
+      "purchase_date": "1/31/2018",
+      "membership": "No",
+      "city": "Issaquah",
+      "state": "WA",
+      "amount": "$28.50"
+    },
+    {
+      "customer_name": "Apple Tran",
+      "purchase_date": "4/22/2018",
+      "membership": "Yes",
+      "city": "Portland",
+      "state": "OR",
+      "amount": "$220.50"
+    },
+    {
+      "customer_name": "Keo Thram",
+      "purchase_date": "9/1/2018",
+      "membership": "Yes",
+      "city": "Portland",
+      "state": "OR",
+      "amount": "$342.39"    
+    },
+    {
+      "customer_name": "William Malik",
+      "purchase_date": "4/30/2018",
+      "membership": "No",
+      "city": "Portland",
+      "state": "OR",
+      "amount": "$220.50"    
+    },
+    {
+      "customer_name": "Samantha Smith",
+      "purchase_date": "2/16/2018",
+      "membership": "Yes",
+      "city": "Portland",
+      "state": "OR",
+      "amount": "$55.50"    
+    },
+    {
+      "customer_name": "Julay Dan",
+      "purchase_date": "8/1/2018",
+      "membership": "Yes",
+      "city": "Seattle",
+      "state": "WA",
+      "amount": "$200.50"    
+    },
+    {
+      "customer_name": "Jeffrey Lee",
+      "purchase_date": "1/1/2018",
+      "membership": "No",
+      "city": "Longview",
+      "state": "OR",
+      "amount": "$23.28"
+    },
+    {
+      "customer_name": "Rhona David",
+      "purchase_date": "2/19/2018",
+      "membership": "No",
+      "city": "Salem",
+      "state": "OR",
+      "amount": "$120.29"
+    },
+    {
+      "customer_name": "Sam Frost",
+      "purchase_date": "5/22/2018",
+      "membership": "Yes",
+      "city": "Burien",
+      "state": "WA",
+      "amount": "$243.64"   
+    },
+    {
+      "customer_name": "Jane Gaines",
+      "purchase_date": "8/3/2018",
+      "membership": "Yes",
+      "city": "Tacoma",
+      "state": "WA",
+      "amount": "$130"    
+    },
+    {
+      "customer_name": "Quinn Flynn",
+      "purchase_date": "10/1/2018",
+      "membership": "No",
+      "city": "Seatac",
+      "state": "WA",
+      "amount": "$200.78"
+    }
+  ]
+}
